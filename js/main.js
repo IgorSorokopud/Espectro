35db3e1acdf237ae00c5a936cb45ddd22ef2b3fd
IgorSorokopud <igorztsorokopud@gmail.com>
2015-08-02 11:10:52 +0300

    Initial Commit.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+;(function ($) {
+	$(document).ready(function() {
+		
+		var self;
+		$('.show-drop-down').toggle(function() {
+			self = $(this).next('ul');
+			self.show();
+		}, function () {
+			self.hide();
+		});
+
+	});
+})(jQuery);
