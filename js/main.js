5c9832ec827cdac695146eaca778ff867075f3aa
Igor <igorztsorokopud@gmail.com>
2016-05-23 21:31:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 ;(function ($) {
 	$(document).ready(function() {
-		
 		var self;
+		
 		$('.show-drop-down').toggle(function() {
 			self = $(this).next('ul');
 			self.show();
 		}, function () {
 			self.hide();
 		});
-
 	});
 })(jQuery);
